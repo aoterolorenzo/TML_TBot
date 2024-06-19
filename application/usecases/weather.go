5199b10aade81c5ce60ac2ac2e05c8321ffd3ac5
Alberto Otero Lorenzo <aoterolorenzo@gmail.com>
2024-06-19 21:16:44 +0200

    feat(antiSpoiler): add antiSpoiler use case

--- application/usecases/weather.go
+++ application/usecases/weather.go
@@ -92,7 +92,7 @@ func getForecastSnapshot(title string, url string, elementsToRemove string, x0 i
 		cu.WithHeadless(),
 
 		// If the webelement is not found within 10 seconds, timeout.
-		cu.WithTimeout(10*time.Second),
+		cu.WithTimeout(30*time.Second),
 	))
 	if err != nil {
 		panic(err)
