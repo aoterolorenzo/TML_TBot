4f94462d94ebea8109891e82a264f2665105687b
Alberto Otero Lorenzo <aoterolorenzo@gmail.com>
2024-06-25 19:20:29 +0200

    feat(lineUp): update lineUp use case

--- application/usecases/spoilers.go
+++ application/usecases/spoilers.go
@@ -3,21 +3,34 @@ package usecases
 import (
 	"TML_TBot/config"
 	"TML_TBot/domain/models"
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"math"
 	"os"
 )
 
+const SPOILERS_CACHE_FILE = "./.cache/messages.ndjson"
+
 type TMLAntiSpoilersController struct {
 	msgID          int
+	msgs           map[int]tgbotapi.Message
 	responseParams models.ResponseParams
 	botToken       string
 }
 
 func NewTMLAntiSpoilersController(job models.Job) *TMLAntiSpoilersController {
+	msgs, err := readMessagesFromNDJSONFile(SPOILERS_CACHE_FILE)
+	if err != nil {
+		panic(errors.Join(errors.New(fmt.Sprintf("unable to open file: %s", SPOILERS_CACHE_FILE)), err))
+	}
+
 	return &TMLAntiSpoilersController{
 		responseParams: job.Response[0],
 		msgID:          math.MinInt64,
+		msgs:           msgs,
 		botToken:       os.Getenv("TOKEN"),
 	}
 }
@@ -37,8 +50,36 @@ func (t *TMLAntiSpoilersController) Run() ([]models.TGMessage, error) {
 	}
 
 	for update := range updates {
+		var currentMsg = update.Message
+		var topicCheckPassed = false
+		if update.Message != nil && update.Message.Chat.ID == int64(t.responseParams.ChatID) {
+			t.msgs[update.Message.MessageID] = *update.Message
+			err := appendMessageToNDJSONFile(SPOILERS_CACHE_FILE, *update.Message)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		for !topicCheckPassed && currentMsg != nil {
+			// if it has father (currentMsg.ReplyToMessage) and ReplyToMessage IS topic
+			if currentMsg.ReplyToMessage != nil && currentMsg.ReplyToMessage.MessageID == int(t.responseParams.TopicID) {
+				// topicCheckPassed, activate spoiler process
+				topicCheckPassed = true
+			} else if currentMsg.ReplyToMessage != nil {
+				// let's grab the father and continue. if we dont have it, break, topicCheckPassed still false
+				if foundFather, exists := t.msgs[currentMsg.ReplyToMessage.MessageID]; exists {
+					currentMsg = &foundFather
+				} else {
+					break
+				}
+			} else {
+				// it doesn't respond to anything. break loop and topicCheckPassed still false
+				break
+			}
+		}
+
 		if update.Message != nil && update.Message.Chat.ID == int64(t.responseParams.ChatID) &&
-			(t.responseParams.TopicID == 0 || update.Message.ReplyToMessage.MessageID == int(t.responseParams.TopicID)) {
+			topicCheckPassed {
 			// If we have a msg id, we just remove the message id
 			if t.msgID != math.MinInt64 {
 				deleteMessage := tgbotapi.DeleteMessageConfig{
@@ -71,4 +112,80 @@ func (t *TMLAntiSpoilersController) Run() ([]models.TGMessage, error) {
 	}
 
 	return nil, nil
+}
+
+func readMessagesFromNDJSONFile(filename string) (map[int]tgbotapi.Message, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	messages := make(map[int]tgbotapi.Message)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		var msg tgbotapi.Message
+		err := json.Unmarshal(scanner.Bytes(), &msg)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal message: %w", err)
+		}
+		messages[msg.MessageID] = msg
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scanner error: %w", err)
+	}
+
+	return messages, nil
+}
+
+func writeMessagesToNDJSONFile(filename string, messages map[int]tgbotapi.Message) error {
+	// Open the file in write mode
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for _, msg := range messages {
+		data, err := json.Marshal(msg)
+		if err != nil {
+			return fmt.Errorf("failed to marshal message: %w", err)
+		}
+
+		_, err = writer.Write(append(data, '\n'))
+		if err != nil {
+			return fmt.Errorf("failed to write to file: %w", err)
+		}
+	}
+
+	if err := writer.Flush(); err != nil {
+		return fmt.Errorf("failed to flush writer: %w", err)
+	}
+
+	return nil
+}
+
+func appendMessageToNDJSONFile(filename string, newMessage tgbotapi.Message) error {
+	// Open the file in append mode
+	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer file.Close()
+
+	// Marshal the new message to JSON
+	data, err := json.Marshal(newMessage)
+	if err != nil {
+		return fmt.Errorf("failed to marshal message: %w", err)
+	}
+
+	// Write the JSON data to the file followed by a newline
+	_, err = file.Write(append(data, '\n'))
+	if err != nil {
+		return fmt.Errorf("failed to write to file: %w", err)
+	}
+
+	return nil
 }
